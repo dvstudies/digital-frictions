960a65a7fb2f866a9d98479520e2bf6c9790b1bf
neriiacopo <neri.iacopo@gmail.com>
2025-06-09 16:45:29 +0200

    add cloudfare settings

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,14 @@ import topLevelAwait from "vite-plugin-top-level-await";
 
 const isCodeSandbox =
     "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
+
+const isCloudflare = process.env.CLOUDFLARE_PAGES === "true";
+
 export default {
     plugins: [react(), topLevelAwait()],
     root: "src",
     publicDir: "public",
-    base: "/digital-frictions/",
+    base: isCloudflare ? "/" : "/digital-frictions/",
     server: {
         host: true,
         open: !isCodeSandbox,
